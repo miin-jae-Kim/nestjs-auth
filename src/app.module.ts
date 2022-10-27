efa391dd06974c35c26dff3bd40230d90cab98d3
miin-jae-Kim <bapbodanbbang@gmail.com>
2022-10-27 14:21:40 +0900

    Add: mysql Connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Connection from 'mysql2/typings/mysql/lib/Connection';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection:Connection) {}
+}
