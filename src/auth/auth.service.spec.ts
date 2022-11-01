c0f60420d07ca8b71c40a87ec1d885be0a23863c
miin-jae-Kim <bapbodanbbang@gmail.com>
2022-11-01 11:09:46 +0900

    Update: auth service test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,44 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/entity/user.entity';
+import { Repository } from 'typeorm';
 import { AuthService } from './auth.service';
 
+
+const mockUserRepository = () => ({
+  save: jest.fn(),
+  find: jest.fn(),
+  findOne: jest.fn(),
+  softDelete: jest.fn(),
+});
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
 describe('AuthService', () => {
-  let service: AuthService;
+
+  let authService: AuthService;
+  let jwtService: JwtService;
+  let userRepository: MockRepository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository()
+        }
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
+    userRepository = module.get<MockRepository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
   });
 });
