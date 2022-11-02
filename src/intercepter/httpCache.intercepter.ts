eda1121825b4e389255cdafb63698fe3867a7844
miin-jae-Kim <bapbodanbbang@gmail.com>
2022-11-02 10:40:24 +0900

    Add: remark, reflector exaplanation

--- src/intercepter/httpCache.intercepter.ts
+++ src/intercepter/httpCache.intercepter.ts
@@ -4,6 +4,16 @@ import { Observable } from "rxjs";
 @Injectable()
 export class HttpCacheInterceptor extends CacheInterceptor {
   trackBy(context: ExecutionContext): string | undefined {
+
+    /*
+      Reflector helper class, which is provided out of the box by the framework and exposed from the @nestjs/core package. 
+      Reflector can be injected into a class in the normal way
+
+
+      The Reflector#get method allows us to easily access the metadata by passing in two arguments: 
+      a metadata key and a context (decorator target) to retrieve the metadata from. 
+    */
+
     const cacheKey = this.reflector.get(
       CACHE_KEY_METADATA,
       context.getHandler(),
